78a1ca78c561e45120e0068018d6e0846f8d1f43
gali gershtein <213337868@idf.il>
2023-09-06 09:21:27 +0300

    change json to format

--- sensor.py
+++ sensor.py
@@ -2,18 +2,22 @@ import asyncio
 import websockets
 import random
 import json
-
+import rstr
 
 async def send_random_messages(websocket):
+    print('hi, i just wanna say hello')
     min_x, max_x = -10, 10
     min_y, max_y = -10, 10
-    
+
     while True:
         my_object = {
-            'x': random.uniform(min_x, max_x),
-            'y': random.uniform(min_y, max_y),
-            'dest': (30, 40),
-            'origin': (0, 0)
+            'lat': random.uniform(min_x, max_x),
+            'long': random.uniform(min_y, max_y),
+            'altitude': random.uniform(0, 13.0),
+            'id': rstr.xeger(r'^[0-9a-z]{2}-[0-9a-z]{2,5}$'),
+            'speed': random.uniform(0, 4024.0),
+            'dst': 'BCN',
+            'src': 'TLV'
         }
         json_data = json.dumps(my_object)
         # x = random.uniform(min_x, max_x)
@@ -23,6 +27,5 @@ async def send_random_messages(websocket):
         await asyncio.sleep(5)  # Send a message every 1 second
 
 start_server = websockets.serve(send_random_messages, "localhost", 8765)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
